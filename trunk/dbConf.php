bbfc20d83f3633baf2b1c73654a0fdc6926d2a4e
gipsaliu@gmail.com <gipsaliu@gmail.com@a2f4c9ff-edf4-7463-d30a-ba759fdd7e78>
2012-08-18 15:32:39 +0000

    修改了配置文件一边能够运行在viivtech上

--- trunk/dbConf.php
+++ trunk/dbConf.php
@@ -5,10 +5,10 @@
  * 不管在何处，它的未知应该在系统配置
  * 文件config.php中用$dbConfFile指明。
  */
-	$host = '127.0.0.1';
-	$db = 'db_name';
-	$user = 'db_user_name';
-	$pwd = 'db_password';
+	$host = 'localhost';
+	$db = 'vdl';
+	$user = 'vdl_user';
+	$pwd = 'asvdl_user';
 
 	global $dbServs;
 	$dbServs = array($host, $db, $user, $pwd);
