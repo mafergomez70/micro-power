3560390b60b8b523e9b35781e99434364bb9b612
gipsaliu@gmail.com <gipsaliu@gmail.com@a2f4c9ff-edf4-7463-d30a-ba759fdd7e78>
2012-09-21 12:48:35 +0000

    user表拆分成了多个表，在思考task表是不是也应该做类似拆分

--- trunk/doc/create_table.sql
+++ trunk/doc/create_table.sql
@@ -8,13 +8,13 @@ CREATE TABLE `user` (
     /* 直接在微动力首页上用微博登录的用户默认为user，ader注册需要邀请链接 */
     status ENUM('normal', 'verified', 'examine', 'forbid') DEFAULT 'normal',
     /* 分别对应：普通，已认证，审核，封禁 */
-	sina_uid BIGINT UNSIGNED,
-	sina_token VARCHAR(50),
-    token_update_at DATETIME,
-    token_expire_in INT UNSIGNED,
+/*	sina_uid BIGINT UNSIGNED, */
+/*	sina_token VARCHAR(50), */
+/*  token_update_at DATETIME, */
+/*    token_expire_in INT UNSIGNED, */
 	level TINYINT default 3,   /*欲更改为level*/ /* 此level由微动力评定user和ader使用不同的评定标准 */
-    sina_location VARCHAR(30),      /* 此location有sina提供 */
-    sina_description VARCHAR(220),  /* form sina */
+/*    sina_location VARCHAR(30),      /* 此location有sina提供 */
+/*    sina_description VARCHAR(220),  /* form sina */
 	pro TINYINT UNSIGNED DEFAULT 0, /* 好评，*/
 	con TINYINT UNSIGNED DEFAULT 0, /* 差评 */
 	task_taken INT UNSIGNED DEFAULT 0,  /* user-承接任务数|ader-发布任务数 */
@@ -22,18 +22,53 @@ CREATE TABLE `user` (
     /* 注意：金钱在数据库内部以 厘 为单位存储 */
     total_income INT UNSIGNED DEFAULT 0,    /* user-总入账金额|ader-总投入|其他留空*/
     realtime_income INT UNSIGNED DEFAULT 0, /* user-当前账户金额|ader-当前账户金额|其他留空 */
-	alipay_id VARCHAR(50) DEFAULT NULL,     /* 支付宝id */
-	alipay_name VARCHAR(50) DEFAULT NULL,   /* 支付宝用户名 */
-    alipay_token VARCHAR(50) DEFAULT NULL,  /* 支付宝token */
+/*	alipay_id VARCHAR(50) DEFAULT NULL,     /* 支付宝id */
+/*	alipay_name VARCHAR(50) DEFAULT NULL,   /* 支付宝用户名 */
+/*    alipay_token VARCHAR(50) DEFAULT NULL,  /* 支付宝token */
     cell_phone CHAR(11),    /* 可用于实名认证，ader注册必填 */
 	reg_time DATETIME NOT NULL,
 	PRIMARY KEY(user_id),
 	UNIQUE (email),
 	UNIQUE (nick_name),
-	UNIQUE (sina_uid),
+/*	UNIQUE (sina_uid), */
 	INDEX (email(25))
 );
-DESCRIBE `user`;
+/* DESCRIBE `user`; */
+
+DROP TABLE IF EXISTS `user_info_sina`;
+CREATE TABLE `user_info_sina` (
+    info_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    sina_uid BIGINT UNSIGNED,
+    sina_token VARCHAR(50),
+    token_update_at DATETIME,
+    token_expire_in INT UNSIGNED,
+    sina_level TINYINT DEFAULT 3,
+    sina_screen_name VARCHAR(60),
+    sina_location VARCHAR(30),
+    sina_description VARCHAR(220),
+    bind_time DATETIME NOT NULL,
+    PRIMARY KEY(info_id),
+    UNIQUE (user_id),
+    UNIQUE (sina_uid),
+    INDEX (sina_uid),
+    INDEX (sina_screen_name)
+);
+/*DESCRIBE `user_info_sina`; */
+
+DROP TABLE IF EXISTS `user_info_ali`;
+CREATE TABLE `user_info_ali` (
+    info_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    ali_uid BIGINT UNSIGNED,
+    ali_name VARCHAR(50),
+    ali_token VARCHAR(50),
+    PRIMARY KEY(info_id),
+    UNIQUE (user_id),
+    UNIQUE (ali_uid),
+    INDEX (ali_name)
+);
+/*DESCRIBE `user_info_ali`; */
 
 DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
@@ -63,7 +98,7 @@ CREATE TABLE `task` (
 	PRIMARY KEY (task_id),
 	INDEX (task_type)
 );
-DESCRIBE `task`;
+/*DESCRIBE `task`; */
 
 DROP TABLE IF EXISTS `do_task`;
 CREATE TABLE `do_task` (
@@ -73,10 +108,10 @@ CREATE TABLE `do_task` (
 	status ENUM('unfinish', 'finish', 'fail', 'retract', 'hide'),
     /* 分别对应：尚未完成，成功完成，失败或屏蔽，反悔，屏蔽*/
     /* repost_mid 仅针对转发任务，为转发产生的微博的mid，十进制 */
-    repost_mid BIGINT UNSIGNED DEFAULT NULL,
+    repost_mid BIGINT UNSIGNED DEFAULT NULL,    -- 此字段放在这里并不太好，暂时如此吧
     time DATETIME NOT NULL,
 	PRIMARY KEY(do_id),
 	INDEX (task_id),
 	INDEX (user_id)
 );
-DESCRIBE `do_task`;
+/*DESCRIBE `do_task`; */
